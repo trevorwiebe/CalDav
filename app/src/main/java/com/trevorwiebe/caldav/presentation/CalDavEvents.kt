428ef3b05ece50696d43e54a1980ddc69e82a56c
trevor <thisistrevor4@gmail.com>
2023-10-22 14:52:35 -0500

    remover CalDavViewModel.kt, add CalendarListViewModel

--- app/src/main/java/com/trevorwiebe/caldav/presentation/CalDavEvents.kt
+++
@@ -1,8 +0,0 @@
-package com.trevorwiebe.caldav.presentation
-
-sealed class CalDavEvents {
-    data class OnUsernameChange(val userName: String): CalDavEvents()
-    data class OnPasswordChange(val password: String): CalDavEvents()
-    data class OnURLChange(val url: String): CalDavEvents()
-    object OnAddCal: CalDavEvents()
-}
